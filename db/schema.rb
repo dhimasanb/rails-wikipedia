6575e85a94e6f1dcff3a04088d61d02e8146f153
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-03 00:08:45 +0700

    db migrate add_user_id_to_articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202165315) do
+ActiveRecord::Schema.define(version: 20170202170743) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
+    t.string   "index"
   end
 
   create_table "users", force: :cascade do |t|
