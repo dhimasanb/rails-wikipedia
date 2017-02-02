4396279fe40c10e7012455456ca91eb732f08e00
Dhimas Akbar N.B <dhimasakbarnoorbaihaque@gmail.com>
2017-02-03 00:19:27 +0700

    db migrate add_category_id_to_articles

--- db/schema.rb
+++ db/schema.rb
@@ -10,15 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170202171644) do
+ActiveRecord::Schema.define(version: 20170202171843) do
 
   create_table "articles", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.integer  "user_id"
     t.string   "index"
+    t.integer  "category_id"
   end
 
   create_table "categories", force: :cascade do |t|
